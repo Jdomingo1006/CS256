b0ed0505183aef2dc02b091604da603556154e02
Jdomingo1006 <35078033+Jdomingo1006@users.noreply.github.com>
2018-04-19 13:50:31 -0700

    Updated Population

--- Homework3/Population.cpp
+++ Homework3/Population.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-	int population, years, newPopulation;
-	double birthRate, deathRate;
+float getPopulation(double, double, double);
 
+int main() {
+	int years;
+	double birthRate, deathRate, population, newPopulation;
+
+	//Getting birthrate/deathrate/population/etc from user
 	cout << "Welcome to Population program\n";
 	cout << "What is the starting size of the population?";
 	cin >> population;
@@ -15,11 +18,22 @@ int main() {
 	cout << "Amount of years to be calculated?";
 	cin >> years;
 
+	//Calculates new population with correct numbers
 	if ((population > 2) && ((birthRate && deathRate) >= 0) && (years >=1)) {
-		newPopulation = population + birthRate * population - deathRate*population;
-		//newPopulation = years * newPopulation;
-		cout << "The new population is " << newPopulation <<"\n";
+		//Shows initial population
+		cout << "\nInitial Population: " << population << "\n";
+		
+		//For-loop to count each year
+		for (int i = 0; i < years; i++) {
+			//Calls "getPopulation" function to calculate a new population
+			newPopulation = getPopulation(population, birthRate, deathRate);
+			//Prints out each year
+			cout << "Population by year " << (i + 1) << ": " << newPopulation << "\n";
+			//Updates population
+			population = newPopulation;
+		}
 	}
+	//Incorrect numbers from user
 	else {
 		cout << "Please enter valid numbers\n";
 		cout << "Starting population should be more than 2.\n";
@@ -27,4 +41,11 @@ int main() {
 		cout << "Minimum number of 1 year for years.\n";
 	}
 	return 0;
+}
+
+//Function that calculates population with birthrate/deathrate
+float getPopulation(double P, double B, double D) {
+	double N;
+	N = P + (B*P) - (D*P);
+	return N;
 }
