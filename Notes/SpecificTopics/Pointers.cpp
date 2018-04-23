e633db7d889f124c32bbb81363a8d59a3fcbd516
Jdomingo1006 <35078033+Jdomingo1006@users.noreply.github.com>
2018-04-23 14:53:22 -0700

    Update Pointers.cpp

--- Notes/SpecificTopics/Pointers.cpp
+++ Notes/SpecificTopics/Pointers.cpp
@@ -3,9 +3,7 @@ using namespace std;
 int main() {
 
 	int* ptr;
-	int* ptr2;
 	int var = 7;
-	int foo = 21;
 	ptr = &var;
 	*ptr = var;
 	cout << "The address of var is: " << ptr << "\n"; // & = the address of. So pointer is pointing at the address of var
