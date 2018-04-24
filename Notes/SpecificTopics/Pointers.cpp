e2a299de0bec9a52c892fe1633d4a036b0be1df2
Jdomingo1006 <35078033+Jdomingo1006@users.noreply.github.com>
2018-04-24 10:59:16 -0700

    Update Pointers.cpp

--- Notes/SpecificTopics/Pointers.cpp
+++ Notes/SpecificTopics/Pointers.cpp
@@ -8,6 +8,9 @@ int main() {
 	*ptr = var;
 	cout << "The address of var is: " << ptr << "\n"; // & = the address of. So pointer is pointing at the address of var
 	cout << "The value of var is: " << *ptr << "\n"; // *ptr gains access to the value of var
+	//Print out:
+	//The address of var is: 1000
+	//The value of var is: 7
 
 	return 0;
 }
