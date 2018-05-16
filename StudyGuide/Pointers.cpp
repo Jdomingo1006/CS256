f0c0355a101f95648f1aa56fcfcc984bf02e41d3
Jdomingo1006 <35078033+Jdomingo1006@users.noreply.github.com>
2018-05-15 17:07:33 -0700

    Add files via upload

---
+++ StudyGuide/Pointers.cpp
@@ -0,0 +1,18 @@
+#include "stdafx.h"
+#include <iostream>
+#include <string>
+/*using namespace std;
+
+int main() {
+	int x = 25;
+	int *ptr = nullptr; //Pointer variable, can point ot an int
+
+	ptr = &x; //Store the address of x in ptr
+
+
+	cout << "The address of x is " << ptr << endl; //Prints out random letters/numbers
+	cout << "Pointer and declassification example: " << (*ptr +5) << endl; //Prints out 30
+	cout << "The size of x is " << sizeof(x) << " bytes\n"; //4 bytes
+	cout << "The value in x is " << x << endl; //Prints out 25
+	return 0;
+}*/
